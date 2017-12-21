a6fe8393c835c01307a0780816137d8db92d8f44
cjcmattsson <cjcmattsson@gmail.com>
2017-12-21 15:04:34 +0100

    Removed stupid fakking wortless json shit

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,39 +2,3 @@
 
 const voteUp = document.querySelector('.voteUp');
 const voteDown = document.querySelector('.voteDown');
-const url = '../vote.php';
-
-function addVoteUp() {
-      fetch(url, {
-        method: "POST",
-        body: `up=${voteUp.value}`,
-        headers: {"Content-type": "application/x-www-form-urlencoded; charset=UTF-8"}
-
-      })
-        .then(response => {
-        return response.json()
-      })
-        .then(json => {
-          // Now we can now use the JSON as a normal object.
-          console.log(parseInt(json));
-        });
-}
-
-function addVoteDown() {
-      fetch(url, {
-        method: "POST",
-        body: `up=${voteDown.value}`,
-        headers: {"Content-type": "application/x-www-form-urlencoded; charset=UTF-8"}
-
-      })
-        .then(response => response.json())
-        .then(json => {
-          // Now we can now use the JSON as a normal object.
-          console.log(json);
-        });
-}
-
-
-voteUp.addEventListener('click', addVoteUp);
-
-voteDown.addEventListener('click', addVoteDown);
--- vote.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-  $voteValue = (int)$_POST["up"];
-
-  echo json_encode($voteValue);
-
- ?>
